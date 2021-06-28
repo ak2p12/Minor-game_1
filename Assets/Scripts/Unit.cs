4f2839e0389c40c5f6e16745ff9f8f21801a8fc1
October_26 <ak2p12@gmail.com>
2021-06-28 17:21:00 +0900

    플레이어 구현중
    캐릭터 움직임 , 점프 , 공격애니메이션 구현중

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public float HP;
+    public float MP;
+    public float SP;
+    public float MoveSpeed;
+    public float JumpPower;
+    public float AttackDamage;
+
+    [HideInInspector] public Rigidbody2D Rigid;
+    [HideInInspector] public Animator Anim;
+    [HideInInspector] public SpriteRenderer Render;
+
+    //void Start()
+    //{
+        
+    //}
+
+    //void Update()
+    //{
+        
+    //}
+}
---
+++ Assets/Scripts/User.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : Unit
+{
+    bool move;
+
+    void Start()
+    {
+        Anim = GetComponent<Animator>();
+        Rigid = GetComponent<Rigidbody2D>();
+        Render = GetComponent<SpriteRenderer>();
+
+        StartCoroutine(Update_Coroutine());
+    }
+    void Update()
+    {
+
+    }
+
+    IEnumerator Update_Coroutine()
+    {
+        while (true)
+        {
+            MoveAction();
+            yield return null;
+        }
+    }
+
+    void MoveAction()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow)) //왼 쪽
+        {
+            Rigid.velocity = new Vector2(Vector2.left.x * (MoveSpeed), Rigid.velocity.y);
+            Render.flipX = true;
+            move = true;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow)) //오른 쪽
+        {
+            Rigid.velocity = new Vector2(Vector2.right.x * (MoveSpeed), Rigid.velocity.y);
+            Render.flipX = false;
+            move = true;
+        }
+        else
+        {
+            move = false;
+        }
+            
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) //위
+        {
+            Rigid.velocity = new Vector2(Rigid.velocity.x, JumpPower);
+            Anim.SetTrigger("Jump");
+        }
+
+        Anim.SetFloat("velocity_Y", Rigid.velocity.y);
+        Anim.SetInteger("velocity_Y_Int", (int)Rigid.velocity.y);
+        Anim.SetBool("Walk", move);
+    }
+    void AttackAction()
+    {
+        if (Input.GetKey(KeyCode.Z)) //공격
+        {
+
+        }
+    }
+}
